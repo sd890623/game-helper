dffbbb2cffc9167c27e065c680d6c4906eec18dc
sd890623 <sd890623@gmail.com>
2022-07-09 22:53:55 +1000

    Update mainThread.py

--- src/mainThread.py
+++ src/mainThread.py
@@ -10,6 +10,7 @@ def runTask(hwnd, index):
         #task.runTask()
         task.startMiningTask()
 def main():
+    print("开工前todo list: 打开本地列表，v船到广角，检查快捷列表有无长字符")
     allWindowsWithTitle = getAllWindowsWithTitle("星战前夜：无烬星河 - MuMu模拟器")
     threads = []
     for index,window in enumerate(allWindowsWithTitle):

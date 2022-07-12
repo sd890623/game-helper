a960192298ba15ff80d2a84f87caf587ef3bb86e
sd890623 <sd890623@gmail.com>
2022-07-13 01:58:02 +1000

    Shorter random action delay

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,7 @@ import threading
 
 def wait(func, seconds = 2):
     func()
-    time.sleep(seconds+random.randint(3,6))
+    time.sleep(seconds+random.randint(0,3))
 
 def getDateTimeString():
     now = datetime.now()
